976e858752f98568e5584691bfb84e3a4af0e586
andrereliquia <147876278+andrereliquia@users.noreply.github.com>
2024-04-18 22:23:05 -0300

    :tada: initial commit

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ function toggleMode() {
   //pegar imagem
   const img = document.querySelector("#profile img")
 
-  if (html.classList.contains('light')) {
+  if (html.classList.contains("light")) {
     //se tiver light-mode, adiciona a imagem light
-    img.setAttribute("src", "./assets/assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     //se tiver sem light-mode, retorna a imagem padrão
-    img.setAttribute("src", "./assets/assets/andre-avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
+}
+
+function getActualYear() {
+  let dataAtual = new Date()
+  let ano = dataAtual.getFullYear()
+  document.getElementById("ano").innerText = ano
 }
